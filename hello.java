c78fb9b816fd0b9812944be88b6972c88a355800
lixiong-0828 <13154267368@163.com>
2025-08-25 22:39:25 +0800

    change 5: changed in local main and up to web branch!

--- hello.java
+++ hello.java
@@ -10,7 +10,8 @@ public class hello {
         System.out.println("hello ~~~ name:" + name);
 	System.out.println("this is test for github branch!!!");
 	System.out.println("this change from branch on github web!!!");	
-	System.out.println("The change 4:  change from domain File hello.java!!!");	
+	System.out.println("The change 4:  change from domain File hello.java!!!");
+	System.out.println("The change 5:  change in local main and up to web branch!!!");	
 
 
         return "Hello World, name:" + name;
